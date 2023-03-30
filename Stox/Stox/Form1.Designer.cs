6c9279f87c81bfc76b268300f767bd794a192722
Alessandro Porpiglia <114176944+alex-211@users.noreply.github.com>
2023-03-30 12:06:41 +0200

    Ricerca e manipolazione link
    progessi su quello. da imparare ad usare i unix time stamp.
    Riguardo la list view, funziona male il clear quindi ci si informerà per l'uso del datagridview. Allegate dispende di Antonellini
    
    Co-Authored-By: 4l3554ndro <125304934+4l3554ndro@users.noreply.github.com>

--- Stox/Stox/Form1.Designer.cs
+++ Stox/Stox/Form1.Designer.cs
@@ -30,20 +30,25 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.DebugPage = new System.Windows.Forms.TabPage();
-            this.LSTdebug = new System.Windows.Forms.ListBox();
             this.listView = new System.Windows.Forms.ListView();
             this.ColLWData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColLWOpen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.ColLWClose = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.ColLWAdjClose = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColLWHigh = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColLWLow = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColLWClose = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColLWAdjClose = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColLWVolume = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DebugPage = new System.Windows.Forms.TabPage();
+            this.LSTdebug = new System.Windows.Forms.ListBox();
+            this.BTrequest = new System.Windows.Forms.Button();
+            this.CBrequest = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.DebugPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -53,55 +58,23 @@
             this.tabControl1.Controls.Add(this.DebugPage);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1067, 554);
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.CBrequest);
+            this.tabPage1.Controls.Add(this.BTrequest);
             this.tabPage1.Controls.Add(this.listView);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(1059, 525);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(1059, 525);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // DebugPage
-            // 
-            this.DebugPage.Controls.Add(this.LSTdebug);
-            this.DebugPage.Location = new System.Drawing.Point(4, 25);
-            this.DebugPage.Margin = new System.Windows.Forms.Padding(4);
-            this.DebugPage.Name = "DebugPage";
-            this.DebugPage.Size = new System.Drawing.Size(1059, 525);
-            this.DebugPage.TabIndex = 2;
-            this.DebugPage.Text = "Debug";
-            this.DebugPage.UseVisualStyleBackColor = true;
-            // 
-            // LSTdebug
-            // 
-            this.LSTdebug.FormattingEnabled = true;
-            this.LSTdebug.ItemHeight = 16;
-            this.LSTdebug.Location = new System.Drawing.Point(12, 5);
-            this.LSTdebug.Margin = new System.Windows.Forms.Padding(4);
-            this.LSTdebug.Name = "LSTdebug";
-            this.LSTdebug.Size = new System.Drawing.Size(1032, 468);
-            this.LSTdebug.TabIndex = 0;
             // 
             // listView
             // 
@@ -114,9 +87,10 @@
             this.ColLWAdjClose,
             this.ColLWVolume});
             this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(8, 7);
+            this.listView.Location = new System.Drawing.Point(6, 6);
+            this.listView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(1044, 510);
+            this.listView.Size = new System.Drawing.Size(784, 342);
             this.listView.TabIndex = 0;
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.View = System.Windows.Forms.View.Details;
@@ -132,16 +106,6 @@
             this.ColLWOpen.Text = "Open";
             this.ColLWOpen.Width = 80;
             // 
-            // ColLWClose
-            // 
-            this.ColLWClose.Text = "Close";
-            this.ColLWClose.Width = 80;
-            // 
-            // ColLWAdjClose
-            // 
-            this.ColLWAdjClose.Text = "Adj Close";
-            this.ColLWAdjClose.Width = 80;
-            // 
             // ColLWHigh
             // 
             this.ColLWHigh.Text = "High";
@@ -152,26 +116,95 @@
             this.ColLWLow.Text = "Low";
             this.ColLWLow.Width = 80;
             // 
+            // ColLWClose
+            // 
+            this.ColLWClose.Text = "Close";
+            this.ColLWClose.Width = 80;
+            // 
+            // ColLWAdjClose
+            // 
+            this.ColLWAdjClose.Text = "Adj Close";
+            this.ColLWAdjClose.Width = 80;
+            // 
             // ColLWVolume
             // 
             this.ColLWVolume.Text = "Volume";
             this.ColLWVolume.Width = 80;
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DebugPage
+            // 
+            this.DebugPage.Controls.Add(this.LSTdebug);
+            this.DebugPage.Location = new System.Drawing.Point(4, 22);
+            this.DebugPage.Name = "DebugPage";
+            this.DebugPage.Size = new System.Drawing.Size(792, 424);
+            this.DebugPage.TabIndex = 2;
+            this.DebugPage.Text = "Debug";
+            this.DebugPage.UseVisualStyleBackColor = true;
+            // 
+            // LSTdebug
+            // 
+            this.LSTdebug.FormattingEnabled = true;
+            this.LSTdebug.Location = new System.Drawing.Point(9, 4);
+            this.LSTdebug.Name = "LSTdebug";
+            this.LSTdebug.Size = new System.Drawing.Size(775, 381);
+            this.LSTdebug.TabIndex = 0;
+            // 
+            // BTrequest
+            // 
+            this.BTrequest.Location = new System.Drawing.Point(8, 371);
+            this.BTrequest.Name = "BTrequest";
+            this.BTrequest.Size = new System.Drawing.Size(75, 23);
+            this.BTrequest.TabIndex = 1;
+            this.BTrequest.Text = "Richiedi dati";
+            this.BTrequest.UseVisualStyleBackColor = true;
+            this.BTrequest.Click += new System.EventHandler(this.BTrequest_Click);
+            // 
+            // CBrequest
+            // 
+            this.CBrequest.FormattingEnabled = true;
+            this.CBrequest.Items.AddRange(new object[] {
+            "AAPL",
+            "SMSN.IL"});
+            this.CBrequest.Location = new System.Drawing.Point(103, 373);
+            this.CBrequest.Name = "CBrequest";
+            this.CBrequest.Size = new System.Drawing.Size(121, 21);
+            this.CBrequest.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Portafoglio Stox";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.DebugPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -191,6 +224,9 @@
         private System.Windows.Forms.ColumnHeader ColLWClose;
         private System.Windows.Forms.ColumnHeader ColLWAdjClose;
         private System.Windows.Forms.ColumnHeader ColLWVolume;
+        private System.Windows.Forms.ComboBox CBrequest;
+        private System.Windows.Forms.Button BTrequest;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Stox/Stox/Form1.cs
+++ Stox/Stox/Form1.cs
@@ -10,11 +10,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using CsvHelper;
+
 using System.Net;
 using System.IO;
 using System.Globalization;
 
-using CsvHelper.Configuration.Attributes; // Questo serve a dare dei nomi custom agli header (es. vedi righa 59) 
+using CsvHelper.Configuration.Attributes; // Questo serve a dare dei nomi custom agli header
 
 
 namespace Stox
@@ -28,35 +29,7 @@ namespace Stox
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Create a web client to request the CSV data
-            var client = new WebClient();
-            var DataStream = client.OpenRead("https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1581498323&period2=1613120723&interval=1d&events=history&includeAdjustedClose=true");
 
-            // Create a reader for CSVhelper
-            var reader = new CsvReader(new StreamReader(DataStream), CultureInfo.InvariantCulture);
-
-            using (reader)
-            {
-                // stores the splitted data in the appl variable
-                var appl = reader.GetRecords<splitter>().ToList();
-
-                foreach (var record in appl)
-                {
-                    //visualizzazione debug 'listbox'
-                    LSTdebug.Items.Add(record.Date + " " + record.Open + " " + record.High + " " + record.Low + " " + record.Close + " " + record.Adj_close + " " + record.Volume);
-
-                    //Creazione tabella 'listview' *migliorabile
-                    ListViewItem item = new ListViewItem(record.Date);
-                    item.SubItems.Add(record.Open);
-                    item.SubItems.Add(record.High);
-                    item.SubItems.Add(record.Low);
-                    item.SubItems.Add(record.Close);
-                    item.SubItems.Add(record.Adj_close);
-                    item.SubItems.Add(record.Volume);
-                    listView.Items.Add(item);
-                }
-                
-            }
         }
 
         // creates a class that splits the csv data 
@@ -71,6 +44,42 @@ namespace Stox
             [Name("Adj Close")]
             public string Adj_close { get; set; }
             public string Volume { get; set; }
+        }
+
+        private void BTrequest_Click(object sender, EventArgs e)
+        {
+//          listView.Clear(); -- sembra non funzionare, fa il clear delle colonne (non solo dei dati) e quindi non permette ai nuovi dati di essere inseriti
+//          ColLWLow.cle -- per le colonne non c'è un metodo .Clear
+
+            // Create a web client to request the CSV data
+            var client = new WebClient();
+            var DataStream = client.OpenRead("https://query1.finance.yahoo.com/v7/finance/download/"+CBrequest.Text+"?period1=1581498323&period2=1613120723&interval=1d&events=history&includeAdjustedClose=true");
+
+            // Create a reader for CSVhelper
+            var reader = new CsvReader(new StreamReader(DataStream), CultureInfo.InvariantCulture);
+
+            using (reader)
+            {
+                // stores the splitted data in the appl variable
+                var appl = reader.GetRecords<splitter>().ToList();
+
+                foreach (var record in appl)
+                {
+                    //visualizzazione debug 'listbox'
+                    LSTdebug.Items.Add(record.Date + " " + record.Open + " " + record.High + " " + record.Low + " " + record.Close + " " + record.Adj_close + " " + record.Volume);
+
+                    //Creazione tabella 'listview' *migliorabile -- metodo archiviato, in pausa. Proviamo un DataGridView (30-3-23)
+                    ListViewItem item = new ListViewItem(record.Date);
+                    item.SubItems.Add(record.Open);
+                    item.SubItems.Add(record.High);
+                    item.SubItems.Add(record.Low);
+                    item.SubItems.Add(record.Close);
+                    item.SubItems.Add(record.Adj_close);
+                    item.SubItems.Add(record.Volume);
+                    listView.Items.Add(item);
+                }
+
+            }
         }
     }
 }
